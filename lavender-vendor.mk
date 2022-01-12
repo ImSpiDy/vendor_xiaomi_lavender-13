84b288a3ac802760d1a2d3e38ae5b8f41654725a
daniml3 <daniel@danielml.dev>
2022-01-12 15:13:19 +0000

    lavender: drop QTI datastatusnotification
    * In some situations this app crashes due to a missing framework
      method on A12, causing network fluctuation with dual sim.
    
    * As it isn't needed, let's just remove it
    
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- lavender-vendor.mk
+++ lavender-vendor.mk
@@ -1739,7 +1739,6 @@ PRODUCT_PACKAGES += \
     TimeService \
     PowerOffAlarm \
     QtiTelephonyService \
-    datastatusnotification \
     imssettings \
     uceShimService \
     dpmserviceapp \
